94d2a4720a12be5a6ca511c582c0fbd04f80b6ae
dkadillak <devin.kadillaka@gmail.com>
2021-09-09 15:04:43 -0600

    bot now will join your voice channel, say a voiceline, then exit voice channel!

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
-import os
+from os import getenv
+from asyncio import sleep
+from math import ceil
+from mutagen.mp3 import MP3
 from discord import VoiceClient, utils, FFmpegPCMAudio
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-FFMPEG_PATH = os.getenv('FFMPEG_PATH')
+TOKEN = getenv('DISCORD_TOKEN')
+FFMPEG_PATH = getenv('FFMPEG_PATH')
+AUDIO_PATH = getenv('AUDIO_PATH')
 
 bot = commands.Bot(command_prefix='>')
 
@@ -19,15 +23,20 @@ async def written_quote(ctx):
     await ctx.send("Have you heard of the high elves?")
 
 
-# https://github.com/adaptlearning/adapt_authoring/wiki/Installing-FFmpeg
-@bot.command(alias=['speak', 'voice'])
+@bot.command(name="speak")
 async def play(ctx):
     audio_channel = ctx.author.voice.channel
-    voice_client = audio_channel.connect
-    audio_path = r'C:\Users\Devin Kadillak\Documents\dev_folder\Sound recordings\oblivion-guard\by-the-9-divines.mp3'
-    audio_source = FFmpegPCMAudio(executable=FFMPEG_PATH, source=audio_path)
+    voice_client = await audio_channel.connect()
+
+    wait_time = ceil(MP3(AUDIO_PATH).info.length)
+
+    audio_source = FFmpegPCMAudio(source=AUDIO_PATH)
+
+    # plays audio clip then leaves after the clip has ended by using length of clip (ceil) in seconds
     if not voice_client.is_playing():
-        voice_client.play(audio_source, after=lambda: voice_client.disconnect())
+        voice_client.play(audio_source)
+        await sleep(wait_time)
+        await voice_client.disconnect()
 
 
 bot.run(TOKEN)
