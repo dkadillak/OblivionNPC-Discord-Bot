69aa30537f5f0976558a76ffbccc987af6f2228a
dkadillak <devin.kadillak@gmail.com>
2021-09-11 13:43:51 -0600

    Added 2 features to the >speak command:
    1. Only accepting the command if the caller is in a voice channel
    2. Only allowing the command to be called once, blocking other calls until finished
    
    Both commands will reply to the caller if the bot was unable to complete the command, either for the caller not being in a voice channel or for currently playing a voice line

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+import inspect
 from os import getenv
 from asyncio import sleep
 from math import ceil
 from mutagen.mp3 import MP3
+from discord.ext.commands.errors import CommandInvokeError
 from discord import VoiceClient, utils, FFmpegPCMAudio
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -25,18 +27,36 @@ async def written_quote(ctx):
 
 @bot.command(name="speak")
 async def play(ctx):
-    audio_channel = ctx.author.voice.channel
-    voice_client = await audio_channel.connect()
 
-    wait_time = ceil(MP3(AUDIO_PATH).info.length)
+    # check if the caller is in a voice channel
+    if ctx.author.voice is None:
+        await ctx.reply("CITIZEN! you need to be in a voice channel first")
 
-    audio_source = FFmpegPCMAudio(source=AUDIO_PATH)
+    else:
 
-    # plays audio clip then leaves after the clip has ended by using length of clip (ceil) in seconds
-    if not voice_client.is_playing():
-        voice_client.play(audio_source)
-        await sleep(wait_time)
-        await voice_client.disconnect()
+        cur_connections = 0
+
+        if ctx.me.voice is not None:
+            cur_connections = 1
+
+        # check if we're already in a voice channel
+        if cur_connections == 0:
+            audio_channel = ctx.author.voice.channel
+
+            voice_client = await audio_channel.connect()
+
+            wait_time = ceil(MP3(AUDIO_PATH).info.length)
+
+            audio_source = FFmpegPCMAudio(source=AUDIO_PATH)
+
+            # plays audio clip then leaves after the clip has ended by using length of clip (ceil) in seconds
+            if not voice_client.is_playing():
+                voice_client.play(audio_source)
+                await sleep(wait_time)
+                await voice_client.disconnect()
+
+        else:
+            await ctx.reply("GAH! i'm currently in a voice channel. Try your command again in a moment citizen. ")
 
 
 bot.run(TOKEN)
