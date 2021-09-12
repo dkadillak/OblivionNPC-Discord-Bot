e3afc138c7f3a76669c24856380ed408832470a3
dkadillak <devin.kadillak@gmail.com>
2021-09-12 14:30:29 -0600

    added support to select which voice line you want to hear, implemented >help command

---
+++ Mp3Indexer.py
@@ -0,0 +1,20 @@
+from os import listdir, getenv
+from os.path import join
+from dotenv import load_dotenv
+from typing import Dict, Tuple
+
+load_dotenv()
+AUDIO_PATH = getenv('AUDIO_PATH')
+
+
+def index_mp3_files() -> Tuple[Dict, Dict]:
+    '''
+    This function indexes all the mp3 files in the .env specified 'AUDIO_PATH'
+    :return: a tuple containing a dictionary of numbers to mp3 files, and a dict of numbers to filenames (for the caller using the help option of the command)
+    '''
+    audio_files = [f.lower() for f in listdir(AUDIO_PATH)]
+    audio_files.sort()
+    mp3_ind_short = zip(range(1, len(audio_files)+1), [mp3.split('.')[0] for mp3 in audio_files])
+    mp3_ind_long = zip(range(1, len(audio_files)+1), [join(AUDIO_PATH, file) for file in audio_files])
+
+    return dict(mp3_ind_short), dict(mp3_ind_long)
--- bot.py
+++ bot.py
@@ -2,61 +2,119 @@ from os import getenv
 from asyncio import sleep
 from math import ceil
 from mutagen.mp3 import MP3
-from discord.ext.commands.errors import CommandInvokeError
-from discord import VoiceClient, utils, FFmpegPCMAudio
+from discord import Embed, FFmpegPCMAudio
 from discord.ext import commands
 from dotenv import load_dotenv
+from random import randrange
+
+from Mp3Indexer import index_mp3_files
+
 
 load_dotenv()
 TOKEN = getenv('DISCORD_TOKEN')
 FFMPEG_PATH = getenv('FFMPEG_PATH')
-AUDIO_PATH = getenv('AUDIO_PATH')
 
-bot = commands.Bot(command_prefix='>')
+mp3_ind_short, mp3_ind_long = index_mp3_files()
+
+bot = commands.Bot(command_prefix='>', help_command=None)
+bot.remove_command('help')
+
+
+def generate_speak_help() -> str:
+    '''
+    function generates the help message for the >speak command
+    :return: string with the details of how to use the command
+    '''
+    no_arg = "\n>speak  =  random voice line\n"
+    numbered_args = "\n".join([f'>speak {key}   =  {value}' for key, value in mp3_ind_short.items()])
+    return f'{numbered_args}{no_arg}'
+
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 
-@bot.command(name='text')
+@bot.command(name='text', help="for testing if the bot is running")
 async def written_quote(ctx):
     await ctx.send("Have you heard of the high elves?")
 
 
-@bot.command(name="speak")
-async def play(ctx):
+@bot.command(name="speak", help=generate_speak_help())
+async def play(ctx, index: int = -1):
 
-    # check if the caller is in a voice channel
-    if ctx.author.voice is None:
-        await ctx.reply("CITIZEN! you need to be in a voice channel first")
-
+    # make sure caller gave me a valid index for the desired voiceline
+    if index not in mp3_ind_long.keys() and index != -1:
+        await ctx.reply("GAH! give me a number in the range of acceptable voice lines. use ``>help`` for more info")
     else:
-
-        cur_connections = 0
-
-        if ctx.me.voice is not None:
-            cur_connections = 1
-
-        # check if we're already in a voice channel
-        if cur_connections == 0:
-            audio_channel = ctx.author.voice.channel
-
-            voice_client = await audio_channel.connect()
-
-            wait_time = ceil(MP3(AUDIO_PATH).info.length)
-
-            audio_source = FFmpegPCMAudio(source=AUDIO_PATH)
-
-            # plays audio clip then leaves after the clip has ended by using length of clip (ceil) in seconds
-            if not voice_client.is_playing():
-                voice_client.play(audio_source)
-                await sleep(wait_time)
-                await voice_client.disconnect()
+        # check if the caller is in a voice channel
+        if ctx.author.voice is None:
+            await ctx.reply("CITIZEN! you need to be in a voice channel first")
 
         else:
-            # bot is already in a voice channel playing a voice line
-            await ctx.reply("GAH! i'm currently in a voice channel. Try your command again in a moment citizen. ")
+            # using -1 as placeholder value for random voice line
+            if index == -1:
+                index = randrange(1, len(mp3_ind_short)+1)
+
+            cur_connections = 0
+
+            if ctx.me.voice is not None:
+                cur_connections = 1
+
+            # check if we're already in a voice channel
+            if cur_connections == 0:
+                audio_channel = ctx.author.voice.channel
+
+                voice_client = await audio_channel.connect()
+
+                wait_time = ceil(MP3(mp3_ind_long[index]).info.length)
+
+                audio_source = FFmpegPCMAudio(source=mp3_ind_long[index])
+
+                # plays audio clip then leaves after the clip has ended by using length of clip (ceil) in seconds
+                if not voice_client.is_playing():
+                    voice_client.play(audio_source)
+                    await sleep(wait_time)
+                    await voice_client.disconnect()
+
+            else:
+                # bot is already in a voice channel playing a voice line
+                await ctx.reply("GAH! i'm currently in a voice channel. Try your command again in a moment citizen. ")
+
+
+@bot.command(name="help", help="gives you more information about each command")
+async def help(ctx, args=None):
+    help_embed = Embed(title="Oblivion NPC")
+    command_names_list = [x.name for x in bot.commands]
+
+    # If there are no arguments, just list the commands:
+    if not args:
+        help_embed.add_field(
+            name="List of supported commands:",
+            value="\n".join(['>'+x.name for x in bot.commands]),
+            inline=False
+        )
+        help_embed.add_field(
+            name="Details",
+            value="Each command starts with '>'\nType `>help -command name-` for more details about each command.",
+            inline=False
+        )
+
+    # If the argument is a command, get the help text from that command:
+    elif args in command_names_list:
+        help_embed.add_field(
+            name=args,
+            value=bot.get_command(args).help
+        )
+
+    # If someone is just trolling:
+    else:
+        help_embed.add_field(
+            name="Nope.",
+            value="Don't think I got that command, boss!"
+        )
+
+    await ctx.send(embed=help_embed)
 
 
 bot.run(TOKEN)
